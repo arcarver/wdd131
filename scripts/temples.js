a33aa92b4434dbe68ca41e6b0d60ec1a73c4147d
arcarver <arcarver25@gmail.com>
2025-05-20 16:21:59 -0700

    before hamburger

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,11 @@ year.innerHTML = `${today.getFullYear()}`;
 
 const lastMod = document.querySelector("#last");
 lastMod.innerHTML = `${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
