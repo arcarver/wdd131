e008117223fa22ccc217133efc4a6a01e1287ab6
arcarver <arcarver25@gmail.com>
2025-05-24 17:55:46 -0700

    large

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,23 @@ year.innerHTML = `${today.getFullYear()}`;
 const lastMod = document.querySelector("#last");
 lastMod.innerHTML = `${document.lastModified}`;
 
+function calculateWindChillCelsius(temperatureCelsius, windSpeedKph) {
+    if (temperatureCelsius >= 10 || windSpeedKph < 4.8) {
+        return "N/A";
+    }
+
+    const windChillCelsius = 13.12 + (0.6215 * temperatureCelsius) - (11.37 * Math.pow(windSpeedKph, 0.16)) + (0.3965 * temperatureCelsius * Math.pow(windSpeedKph, 0.16));
+    return Math.round(windChillCelsius);
+}
+
+
+const temperature = 17; // Celsius
+const windSpeed = 5; // km/h
+const windChill = calculateWindChillCelsius(temperature, windSpeed);
+
+if (typeof windChill === 'number') {
+    console.log(`${windChill}°C`);
+} else {
+    console.log(windChill);
+}
+ 
