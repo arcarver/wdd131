ed715a9504eba433971c630cb00d93c546728e39
arcarver <arcarver25@gmail.com>
2025-06-05 20:55:21 -0700

    naming files

---
+++ week04/scripts/filtered-temples.js
@@ -0,0 +1,15 @@
+const year = document.querySelector("#year");
+const today = new Date();
+
+year.innerHTML = `${today.getFullYear()}`;
+
+const lastMod = document.querySelector("#last");
+lastMod.innerHTML = `${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
