3acd892cbd1d6971c0bfe02ebfa3c4a8db6c2c3b
arcarver <arcarver25@gmail.com>
2025-06-13 18:20:50 -0700

    java

--- scripts/review.js
+++ scripts/review.js
@@ -3,7 +3,7 @@ const showVisits = document.querySelector(".visits");
 let totalVisits = Number(window.localStorage.getItem("totalVisits-ls")) || 0;
 
 if (totalVisits !== 0) {
-    showVisits.innerHTML = totalVisits;
+    showVisits.innerHTML = `You have reviewed ${totalVisits} products. Thank you!`;
 } else {
     showVisits.innerHTML = "This is your first review. Thank you for reviewing our products!"
 }
