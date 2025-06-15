e44bcc6a74cd8e6f21c855777e66a110e123f723
arcarver <arcarver25@gmail.com>
2025-06-14 17:43:14 -0700

    future and home basic

--- project/scripts/garden.js
+++ project/scripts/garden.js
@@ -1,5 +1,74 @@
+// Footer
 const year = document.querySelector("#year");
 const today = new Date();
 year.innerHTML = `${today.getFullYear()}`;
 const lastMod = document.querySelector("#last");
 lastMod.innerHTML = `${document.lastModified}`;
+
+
+// famous gardens through the ages sorted
+const gardens = [
+    {
+        gardenName: "Garden of Eden",
+        location: "Adam-on-dioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL: "https://www.churchofjesuschrist.org/imgs/62321966b50bc66a19fcc6cff584a45fee1eb9ea/full/1920%2C/0/default"
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    },
+    {
+        gardenName: "Garden of Eden",
+        location: "Adamondioman, Missouri",
+        built: 7000 bc
+        area:
+        imageURL:
+    }
+];
