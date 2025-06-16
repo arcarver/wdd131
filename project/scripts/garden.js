fa263be2aa7d83815d9b771719e7d64a252a7a52
arcarver <arcarver25@gmail.com>
2025-06-16 12:17:57 -0700

    verseailles

--- project/scripts/garden.js
+++ project/scripts/garden.js
@@ -9,10 +9,10 @@ lastMod.innerHTML = `${document.lastModified}`;
 // famous gardens through the ages sorted
 const gardens = [
     {
-        gardenName: "Garden of Eden",
-        location: "Adam-on-dioman, Missouri",
-        built: 7000 bc
-        area:
+        gardenName: "Gardens of Versailles",
+        location: "Versailles, France",
+        built: 1661,
+        area: 2000 acres,
         imageURL: "https://www.churchofjesuschrist.org/imgs/62321966b50bc66a19fcc6cff584a45fee1eb9ea/full/1920%2C/0/default"
     },
     {
